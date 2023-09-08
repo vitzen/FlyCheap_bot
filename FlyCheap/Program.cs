0e6efeec21106a1cdad8d12fd1d8f4fcef94030b
Vitalii Zenkov <47973105+vitzen@users.noreply.github.com>
2023-09-08 16:54:20 +0400

    #add SendMessage Method #add change structure of file Program.cs

--- FlyCheap/Program.cs
+++ FlyCheap/Program.cs
@@ -14,114 +14,71 @@ namespace FlyCheap
 {
     class Program
     {
+        private static TelegramBotClient botClient;
+
         public static void Main()
         {
             // Тест коннекта с ботом TG
-            var botClient = new TelegramBotClient("5880963661:AAGGZLU75KJbrE_k-JPRckvCTR9ainZL1wE");
+            botClient = new TelegramBotClient("5880963661:AAGGZLU75KJbrE_k-JPRckvCTR9ainZL1wE");
             var me = botClient.GetMeAsync().Result;
             Console.WriteLine($"Тест коннекта с ботом телеграмма \n" +
                               $"i'm user {me.Id} \n" +
                               $"my name is {me.FirstName} \n" +
                               $"username {me.Username}");
-            
-            
 
-            //     using CancellationTokenSource cts = new();
-            //
-            //     // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
-            //     ReceiverOptions receiverOptions = new()
-            //     {
-            //         AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
-            //     };
-            //     botClient.StartReceiving(Update, Error);
-            //
-            //     Console.ReadLine();
-            // }
-            //
-            // async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken cts)
-            // {
-            //     var message = update.Message;
-            //     if (message.Text != null)
-            //     {
-            //         if (message.Text.ToLower().Contains("Привет"))
-            //         {
-            //             await botClient.SendTextMessageAsync(message.Chat.Id,
-            //                 "Привет, это сервис для поиска дешевых авиарейсов");
-            //             return;
-            //         }
-            //     }
-            // }
-            //
-            //
-            // private static Task Error(ITelegramBotClient botClient, Exception e, CancellationToken cts)
-            // {
-            //     throw new NotImplementedException();
-            // }
+            StartReceiver();
+            Console.ReadLine();
+        }
 
+        /// <summary>
+        /// Метод SatrtReciever для запуска telegram бота
+        /// </summary>
+        public static async Task StartReceiver()
+        {
+            var token = new CancellationTokenSource();
+            var canceltoken = token.Token;
+            var ReOpt = new ReceiverOptions() { AllowedUpdates = { } };
+            await botClient.ReceiveAsync(OnMessage, ErrorMessage, ReOpt, canceltoken);
+        }
 
-            // public static async Task Main()
-            // {
-            //     var botClient = new TelegramBotClient("5880963661:AAGGZLU75KJbrE_k-JPRckvCTR9ainZL1wE");
-            //
-            //     using CancellationTokenSource cts = new();
-            //
-            //     // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
-            //     ReceiverOptions receiverOptions = new()
-            //     {
-            //         AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
-            //     };
-            //
-            //     botClient.StartReceiving(
-            //         updateHandler: HandleUpdateAsync,
-            //         pollingErrorHandler: HandlePollingErrorAsync,
-            //         receiverOptions: receiverOptions,
-            //         cancellationToken: cts.Token
-            //     );
-            //
-            //     var me = await botClient.GetMeAsync();
-            //
-            //     Console.WriteLine($"Start listening for @{me.Username}");
-            //     Console.ReadLine();
-            //
-            //     // Send cancellation request to stop bot
-            //     cts.Cancel();
-            //
-            //     async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
-            //         CancellationToken cancellationToken)
-            //     {
-            //         // Only process Message updates: https://core.telegram.org/bots/api#message
-            //         if (update.Message is not { } message)
-            //             return;
-            //         // Only process text messages
-            //         if (message.Text is not { } messageText)
-            //             return;
-            //
-            //         var chatId = message.Chat.Id;
-            //
-            //         Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
-            //
-            //         // Echo received message text
-            //         Message sentMessage = await botClient.SendTextMessageAsync(
-            //             chatId: chatId,
-            //             text: "You said:\n" + messageText,
-            //             cancellationToken: cancellationToken);
-            //     }
-            //
-            //     Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception,
-            //         CancellationToken cancellationToken)
-            //     {
-            //         var ErrorMessage = exception switch
-            //         {
-            //             ApiRequestException apiRequestException
-            //                 => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-            //             _ => exception.ToString()
-            //         };
-            //
-            //         Console.WriteLine(ErrorMessage);
-            //         return Task.CompletedTask;
-            //     }
+        /// <summary>
+        /// Метод OnMessage
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="update"></param>
+        /// <param name="cts"></param>
+        public static async Task OnMessage(ITelegramBotClient botClient, Update update, CancellationToken cancellation)
+        {
+            if (update.Message is Message message)
+            {
+                SendMessage(message);
+            }
+        }
 
-            Console.ReadKey();
+        /// <summary>
+        /// Метод вывода ошибок Error
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="e"></param>
+        /// <param name="cts"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static async Task ErrorMessage(ITelegramBotClient telegramBot, Exception e,
+            CancellationToken cancelation)
+        {
+            if (e is ApiRequestException requestException)
+            {
+                await botClient.SendTextMessageAsync("", e.Message.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Метод посыла сообщения от бота (ответ олт бота)
+        /// </summary>
+        /// <param name="message"></param>
+        public static async Task SendMessage(Message message)
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, "Hello my Friend, I'm FlyCheap Service");
         }
     }
 }
